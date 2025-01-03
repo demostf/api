32c2a87ed068e7b6610aa5e0bc36255d1675fd2e
Robin Appelman <robin@icewind.nl>
2025-01-04 00:09:56 +0100

    php 8.3

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -3,7 +3,7 @@ final: prev: {
   demostf-api = final.callPackage ./package.nix {};
   demostf-api-dev = final.callPackage ./package.nix {dev = true;};
   demostf-api-test = final.callPackage ./api-test.nix {};
-  demostf-api-php = final.php82.buildEnv {
+  demostf-api-php = final.php83.buildEnv {
     extraConfig = "memory_limit = 2G";
     extensions = {
       enabled,
